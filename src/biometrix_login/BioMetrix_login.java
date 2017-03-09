ea2ff6b9ba0f882c9faf7d9dc45577214f45513c
Saarrah_Isthikar <Saarrah_Isthikar@DESKTOP-9MHQFAP>
2017-03-09 11:42:43 +0530

    Final product

--- src/biometrix_login/BioMetrix_login.java
+++ src/biometrix_login/BioMetrix_login.java
@@ -2,11 +2,10 @@ package biometrix_login;
 
 import java.sql.SQLException;
 
-
 public class BioMetrix_login {
-    
-     public static void main(String args[]) throws ClassNotFoundException, SQLException{
-       
+
+    public static void main(String args[]) throws ClassNotFoundException, SQLException {
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
 
@@ -14,9 +13,9 @@ public class BioMetrix_login {
 
             }
         });
-        
+
         DatabaseHandler.initialize();
-        
+
     }
 
 }
